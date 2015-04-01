a7f45278ee6f71823e91fcdb601fce39f9d47eb5
AnnaPhillips <annaphillips@gmail.com>
2015-04-01 13:00:47 -0400

    Fixed some stuff, other stuff wonky

--- TDSE.py
+++ TDSE.py
@@ -22,30 +22,35 @@ class TDSE:
 
     #Build NxN tridiagonal matrix to represent the lhs of equation.
     def getA(self):
-        a = -2*np.ones(self.N)/self.delx**2-complex(0,1)*np.ones(self.N)/self.delt
+        a = -2*np.ones(self.N)/self.delx**2-1j*np.ones(self.N)/self.delt
         a_sub = np.ones(self.N)
         data = [a_sub,a,a_sub]
         diags = [-1,0,1]
         A = sparse.spdiags(data,diags,self.N,self.N)
+        print A
         return A
+
 
 
     def run(self):
         for n in range(self.N):
-            b = np.zeros(self.N)
+            print("start of a time step")
+            print(self.matrixReal)
+            print(self.matrixImag)
+            b = np.zeros(self.N,dtype=complex)
             Adense = self.getA().todense()
+            print(Adense)
             for i in range(self.N):
-                #This is where it seems to have trouble:
-                b[i-1] = -1.0/self.delt*complex(self.matrixImag[n-1,i-1],
-                                           self.matrixReal[n-1,i-1])
+                b[i] = -1.0/self.delt*(self.matrixImag[n,i]+1j*self.matrixReal[n,i])
+            print("b=")
+            print(b)
             solution = np.linalg.solve(Adense,b)
-            self.matrixReal[n,:] = solution.real
-            self.matrixImag[n,:] = solution.imag
+            print(solution)
+            self.matrixReal[n+1,:] = solution.real
+            self.matrixImag[n+1,:] = solution.imag
+            print(self.matrixReal)
+            print(self.matrixImag)
+            
 
         np.savetxt(self.outputFile + "_Real", self.matrixReal, delimiter=",")
         np.savetxt(self.outputFile + "_Imag", self.matrixImag, delimiter=",")
-
-
-
-
-
