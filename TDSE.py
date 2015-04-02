0d84d2d04d0bba390b5ffb1247e9051536211727
AnnaPhillips <annaphillips@gmail.com>
2015-04-02 12:06:57 -0400

    Changed the code so that we can run with periodic and non-periodic boundary conditions and with potentials

--- TDSE.py
+++ TDSE.py
@@ -4,10 +4,11 @@ from scipy import sparse
 
 class TDSE:
 
-    #input: an array of size 1xN with the initial position, delta x, delta t, "output
+    #input: an array of size 1xN with the initial position, potential, delta x, delta t, "output
     #file name"
-    def __init__(self, initialPos, delx, delt, timesteps, outputFile):
+    def __init__(self, initialPos, potential , delx, delt, timesteps, outputFile):
         self.N = len(initialPos)
+        self.V=potential
         self.timesteps=timesteps
         self.delx = delx
         self.delt = delt
@@ -28,8 +29,34 @@ class TDSE:
         data = [a_sub,a,a_sub]
         diags = [-1,0,1]
         A = sparse.spdiags(data,diags,self.N,self.N)
+        print "original A"
         print A
         return A
+
+    def getAwithVnonperiodic(self):
+        A=np.zeros([self.N,self.N],dtype=complex)
+        A[0,0]=-2/self.delx**2-1j +self.V[0]
+        A[0,1]=1
+        A[self.N-1,self.N-2]= 1
+        A[self.N-1,self.N-1]=-2/self.delx**2-1j +self.V[self.N-1]
+        for i in range(self.N-2):
+            A[i+1,i]=1
+            A[i+1,i+1]=-2/self.delx**2-1j +self.V[i+1]
+            A[i+1,i+2]=1
+        print "A with V non periodic"
+        print A
+        return A
+
+    def getAwithVperiodic(self):
+        A=np.zeros([self.N,self.N],dtype=complex)
+        for i in range(self.N):
+            A[i,(i-1) % self.N]=1
+            A[i,i % self.N]=-2/self.delx**2-1j +self.V[i]
+            A[i,(i+1) % self.N]=1
+        print "A with V periodic"
+        print A
+        return A
+            
 
     def energy(self):
         normalization=1./(self.N)
@@ -47,14 +74,13 @@ class TDSE:
         return Elist
 
 
-    def run(self):
+    def runNonPeriodicNoV(self):
         for n in range(self.timesteps):
             print("start of a time step")
             print(self.matrixReal)
             print(self.matrixImag)
             b = np.zeros(self.N,dtype=complex)
             Adense = self.getA().todense()
-            print(Adense)
             for i in range(self.N):
                 b[i] = -1.0/self.delt*(self.matrixImag[n,i]+1j*self.matrixReal[n,i])
             print("b=")
@@ -70,3 +96,47 @@ class TDSE:
         np.savetxt(self.outputFile + "_RealFiniteDifference.csv", self.matrixReal, delimiter=",")
         np.savetxt(self.outputFile + "_ImagFiniteDifference.csv", self.matrixImag, delimiter=",")
 
+
+    def runNonPeriodicWithV(self):
+        for n in range(self.timesteps):
+            print("start of a time step")
+            print(self.matrixReal)
+            print(self.matrixImag)
+            b = np.zeros(self.N,dtype=complex)
+            A = self.getAwithVnonperiodic()
+            for i in range(self.N):
+                b[i] = -1.0/self.delt*(self.matrixImag[n,i]+1j*self.matrixReal[n,i])
+            print("b=")
+            print(b)
+            solution = np.linalg.solve(A,b)
+            print(solution)
+            self.matrixReal[n+1,:] = solution.real
+            self.matrixImag[n+1,:] = solution.imag
+            print(self.matrixReal)
+            print(self.matrixImag)
+        hopefullythisworks=self.energy()
+        print(hopefullythisworks)
+        np.savetxt(self.outputFile + "_RealFiniteDifference.csv", self.matrixReal, delimiter=",")
+        np.savetxt(self.outputFile + "_ImagFiniteDifference.csv", self.matrixImag, delimiter=",")
+
+    def runPeriodicWithV(self):
+        for n in range(self.timesteps):
+            print("start of a time step")
+            print(self.matrixReal)
+            print(self.matrixImag)
+            b = np.zeros(self.N,dtype=complex)
+            A = self.getAwithVperiodic()
+            for i in range(self.N):
+                b[i] = -1.0/self.delt*(self.matrixImag[n,i]+1j*self.matrixReal[n,i])
+            print("b=")
+            print(b)
+            solution = np.linalg.solve(A,b)
+            print(solution)
+            self.matrixReal[n+1,:] = solution.real
+            self.matrixImag[n+1,:] = solution.imag
+            print(self.matrixReal)
+            print(self.matrixImag)
+        hopefullythisworks=self.energy()
+        print(hopefullythisworks)
+        np.savetxt(self.outputFile + "_RealFiniteDifference.csv", self.matrixReal, delimiter=",")
+        np.savetxt(self.outputFile + "_ImagFiniteDifference.csv", self.matrixImag, delimiter=",")
