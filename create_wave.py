a602c8d9e8a36cc2a37894c67f9a542dd5a0e0d2
adeben <andrew.debenedictis@tufts.edu>
2015-04-06 18:12:12 -0400

    simple functions for xMesh, a gaussian wave, and a scatter plot

---
+++ create_wave.py
@@ -0,0 +1,22 @@
+#! /usr/bin/python
+
+import numpy as np
+import matplotlib.pyplot as plt
+
+
+def xMesh(xMin, xMax, gridpoints):
+    return np.array([xMin + i*(xMax-xMin)/gridpoints for i in range(gridpoints+1)])
+
+
+def gaussian(x0, k0, var):
+    initialPos = (var**2/(2*np.pi))**0.25*np.exp(0.25*(xAR - x0)*(4.j*k0 + (-xAR + x0)*var**2))
+    return initialPos
+
+xAR = xMesh(-10., 10., 200) #make sure the bounds are float types
+wave = gaussian(0, 0, 1)
+
+print xAR
+print wave
+
+plt.scatter(xAR, wave)
+plt.show()
