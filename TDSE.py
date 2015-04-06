d44627500919f2a3f3517988a7e13792f26cbd7c
AnnaPhillips <annaphillips@gmail.com>
2015-04-06 17:57:29 -0400

    added eigensystem calculation

--- TDSE.py
+++ TDSE.py
@@ -4,9 +4,9 @@ from scipy import sparse
 
 class TDSE:
 
-    #input: an array of size 1xN with the initial position, potential, delta x, delta t, "output
+    #input: an array of size 1xN with the initial position, potential, delta x, delta t, time steps, periodic, "output
     #file name"
-    def __init__(self, initWaveFunc, potential , delx, delt, timesteps, outputFile):
+    def __init__(self, initWaveFunc, potential, delx, delt, timesteps, periodic, outputFile):
         self.N = len(initWaveFunc)
         self.V=potential
         self.timesteps=timesteps
@@ -20,20 +20,10 @@ class TDSE:
         #our initWaveFunc array (if the array is all real, it will be all zeros)
         self.matrixImag = np.vstack([initWaveFunc.imag,self.zeroMatrix])
         self.outputFile = outputFile
+        self.periodic=periodic
 
 
-    #Build NxN tridiagonal matrix to represent the lhs of equation.
-    def getA(self):
-        a = 2*np.ones(self.N)/self.delx**2-1j*np.ones(self.N)/self.delt
-        a_sub = np.ones(self.N)
-        data = [-1*a_sub,a,-1*a_sub]
-        diags = [-1,0,1]
-        A = sparse.spdiags(data,diags,self.N,self.N)
-        print "original A"
-        print A
-        return A
-
-    def getAwithVnonperiodic(self):
+    def getAnonPeriodic(self):
         A=np.zeros([self.N,self.N],dtype=complex)
         A[0,0]=-2/self.delx**2-1j +self.V[0]
         A[0,1]=1
@@ -43,20 +33,31 @@ class TDSE:
             A[i+1,i]=-1
             A[i+1,i+1]=+2/self.delx**2-1j +self.V[i+1]
             A[i+1,i+2]=-1
-        print "A with V non periodic"
+        print "A is non periodic"
         print A
         return A
 
-    def getAwithVperiodic(self):
+    def getAperiodic(self):
         A=np.zeros([self.N,self.N],dtype=complex)
         for i in range(self.N):
             A[i,(i-1) % self.N]=-1
             A[i,i % self.N]=2/self.delx**2-1j +self.V[i]
             A[i,(i+1) % self.N]=-1
-        print "A with V periodic"
+        print "A is periodic"
         print A
         return A
-            
+
+    def getEigenStates(self):
+        if self.periodic:
+            A=self.getAperiodic()
+        else:
+            A=self.getAnonPeriodic()
+        Energies,Evectors=np.linalg.eig(A)
+        print "energies"
+        print Energies
+        print "eigenvector"
+        print Evectors
+        
 
     def energy(self):
         normalization=1./(self.N)
@@ -74,36 +75,17 @@ class TDSE:
         return Elist
 
 
-    def runNonPeriodicNoV(self):
+    def run(self):
+        if self.periodic:
+            A=self.getAperiodic()
+        else:
+            A=self.getAnonPeriodic()
+        self.getEigenStates()
         for n in range(self.timesteps):
             print("start of a time step")
             print(self.matrixReal)
             print(self.matrixImag)
             b = np.zeros(self.N,dtype=complex)
-            Adense = self.getA().todense()
-            for i in range(self.N):
-                b[i] = -1.0/self.delt*(self.matrixImag[n,i]+1j*self.matrixReal[n,i])
-            print("b=")
-            print(b)
-            solution = np.linalg.solve(Adense,b)
-            print(solution)
-            self.matrixReal[n+1,:] = solution.real
-            self.matrixImag[n+1,:] = solution.imag
-            print(self.matrixReal)
-            print(self.matrixImag)
-        hopefullythisworks=self.energy()
-        print(hopefullythisworks)
-        np.savetxt(self.outputFile + "_RealFiniteDifference.csv", self.matrixReal, delimiter=",")
-        np.savetxt(self.outputFile + "_ImagFiniteDifference.csv", self.matrixImag, delimiter=",")
-
-
-    def runNonPeriodicWithV(self):
-        for n in range(self.timesteps):
-            print("start of a time step")
-            print(self.matrixReal)
-            print(self.matrixImag)
-            b = np.zeros(self.N,dtype=complex)
-            A = self.getAwithVnonperiodic()
             for i in range(self.N):
                 b[i] = -1.0/self.delt*(self.matrixImag[n,i]+1j*self.matrixReal[n,i])
             print("b=")
@@ -119,24 +101,3 @@ class TDSE:
         np.savetxt(self.outputFile + "_RealFiniteDifference.csv", self.matrixReal, delimiter=",")
         np.savetxt(self.outputFile + "_ImagFiniteDifference.csv", self.matrixImag, delimiter=",")
 
-    def runPeriodicWithV(self):
-        for n in range(self.timesteps):
-            print("start of a time step")
-            print(self.matrixReal)
-            print(self.matrixImag)
-            b = np.zeros(self.N,dtype=complex)
-            A = self.getAwithVperiodic()
-            for i in range(self.N):
-                b[i] = -1.0/self.delt*(self.matrixImag[n,i]+1j*self.matrixReal[n,i])
-            print("b=")
-            print(b)
-            solution = np.linalg.solve(A,b)
-            print(solution)
-            self.matrixReal[n+1,:] = solution.real
-            self.matrixImag[n+1,:] = solution.imag
-            print(self.matrixReal)
-            print(self.matrixImag)
-        hopefullythisworks=self.energy()
-        print(hopefullythisworks)
-        np.savetxt(self.outputFile + "_RealFiniteDifference.csv", self.matrixReal, delimiter=",")
-        np.savetxt(self.outputFile + "_ImagFiniteDifference.csv", self.matrixImag, delimiter=",")
