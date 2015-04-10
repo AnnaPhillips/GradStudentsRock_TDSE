164051245f2fa004fd569f14b0ad2a8aa5adafe2
AnnaPhillips <annaphillips@gmail.com>
2015-04-10 14:03:08 -0400

    Changed the way we're using some of the potentials

--- create_wave.py
+++ create_wave.py
@@ -9,12 +9,12 @@ def xMesh(xMin, xMax, gridpoints):
     return np.array([xMin + i*(xMax-xMin)/gridpoints for i in range(gridpoints+1)])
 
 
-def gaussian(x0, k0, var):
+def gaussian(x0, k0, var, xAR):
     initialPos = (var**2/(2*np.pi))**0.25*np.exp(0.25*(xAR - x0)*(4.j*k0 + (-xAR + x0)*var**2))
     return initialPos
 
-xAR = xMesh(-10., 10., 200) #make sure the bounds are float types
-wave = gaussian(0, 0, 1)
+#xAR = xMesh(-10., 10., 200) #make sure the bounds are float types
+#wave = gaussian(0, 0, 1)
 
 #print xAR
 #print wave
