398863ff1de105c020ca6e967b750086dd296e4a
AnnaPhillips <annaphillips@gmail.com>
2015-04-10 10:16:26 -0400

    Added additional comments and the amplitude to potentials.

--- potentials.py
+++ potentials.py
@@ -3,7 +3,10 @@ import numpy as np
 import matplotlib.pyplot as plt
 
 
+#This file creates arrays for each type of potential we might want to use. We call on these in our RunTests.py file and the results get pushed through the calculations done by TDSE.py. 
+
 class Potentials:
+    #The inputs for this class of objects are the minimum and maximum spacial points, the number of spacial grid points and an overall amplitude factor for the potential. 
     def __init__(self, xMin, xMax, gridpoints, amplitude):
         self.xMin = xMin
         self.xMax = xMax
@@ -12,24 +15,25 @@ class Potentials:
         self.xMesh = np.array([self.xMin + i*(self.xMax-self.xMin)/float(self.gridpoints) for i in range(gridpoints+1)])
 
 
+    #The free particle potential simply creates an array of zeros that is the length of our grid points. 
     def freeParticle(self):
         return self.xMesh*0
         #plt.plot(self.xMesh,self.xMesh**2)
         #plt.show()
 
-
+    #The Harmonic Oscillator potential creates a grid of values A*x**2 for all of our grid points. Note that this means the center of our oscillator is fixed at x=0. This should be used with non-periodic boundary conditions. 
     def harmonicOscillator(self):
-        return self.xMesh**2
+        return self.A*self.xMesh**2
         #plt.plot(self.xMesh,self.xMesh**2)
         #plt.show()
 
-
+    #The triangle potential creates a grid of values A*abs(x) for all of our grid points. Note that this means the center of our triangle is fixed at x=0. This should be used with non-periodic boundary conditions.
     def triangle(self):
-        return np.abs(self.xMesh)
+        return self.A*np.abs(self.xMesh)
         #plt.plot(self.xMesh,np.abs(self.xMesh))
         #plt.show()
 
-
+    #This creates a barrier with a defined width centered on X=0. This should be used with non-periodic boundary conditions. 
     def barrier(self, barWidth):
         #enter width as float
         list = np.zeros([self.gridpoints+1])
@@ -41,7 +45,7 @@ class Potentials:
         #plt.plot(self.xMesh,list)
         #plt.show()
 
-
+    #This creates a semi-infinite square well of a defined barrier width centered on X=0. This should be used with our non-periodic boundary conditions.
     def squareWell(self, barWidth):
         #enter width as float
         list = np.zeros([self.gridpoints+1])
@@ -53,7 +57,7 @@ class Potentials:
         #plt.plot(self.xMesh,list)
         #plt.show()
 
-
+    #This defines our "saw tooth" potential. It should be used with the periodic boundary conditions. 
     def kronigPenney(self, gapWidth, numberWells):
         list = np.zeros([self.gridpoints+1])
         tooth=(self.xMax - self.xMin)/numberWells
@@ -65,12 +69,12 @@ class Potentials:
         #plt.plot(self.xMesh,list)
         #plt.show()
                 
-                
+    #This defines an imaginary potential. It should be used with the non-periodic boundary conditions.            
     def imagPotential(self):
         # V=ix
-        return 1.j*self.xMesh
+        return self.A*1.j*self.xMesh
         
-        
+    #This defines another potential. It should be used with the non-periodic boundary conditions.            
     def complexPotential(self):
         # V=x+ix
         return self.xMesh + 1.j*self.xMesh
